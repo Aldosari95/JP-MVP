e1625f518df0bfd638c3bf097fbbc7e7f3c3c35a
Mohammed Aldosari <Mohammed1-95@Mohammed-Al-Dosaris-MacBook-Pro.local>
2016-06-13 00:13:26 +0300

    Fixed some issues regsrding the timeline

--- Timeline/ajax-content.php
+++ Timeline/ajax-content.php
@@ -156,14 +156,7 @@ switch($id) {
 			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</div>';
 	break;
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 }
 ?>
